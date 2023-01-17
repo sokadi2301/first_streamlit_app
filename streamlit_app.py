6427a6b924f55861c168eb826822d8fe350e8797
Soukaina Kadi <122612514+sokadi2301@users.noreply.github.com>
2023-01-17 10:04:00 +0100

    import another python library called requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits :", list(my_fruit_list
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
-
+#new section to display fruityvice api response
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
